b66c2cd074dadf7ff41ca93061bc70d745f1ee82
TMaszko <t.krzyzowski96@gmail.com>
2016-06-27 22:34:43 +0200

    Update destroyer.js

--- Exercise/Destroyer/destroyer.js
+++ Exercise/Destroyer/destroyer.js
@@ -1,11 +1,11 @@
 function destroyer(arr) {
     var args = arguments;
     return arr.filter(function(el) {
-        return numToDestroy(args, el);
+        return isNotANumToDestroy(args, el);
     })
 }
 
-function numToDestroy(arr,valueToDestroy){
+function isNotANumToDestroy(arr,valueToDestroy){
     for(var i = 1 ; i < arr.length; i++)
         if (arr[i] == valueToDestroy) { // check if args equal to element in orginal array
             return false;           // because of filter method
