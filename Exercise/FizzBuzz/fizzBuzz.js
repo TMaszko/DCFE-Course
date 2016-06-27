fa373bb7ae0bef0a6cea4622d51b5a961bc772e4
TMaszko <t.krzyzowski96@gmail.com>
2016-06-27 17:45:05 +0200

    Update fizzBuzz.js

--- Exercise/FizzBuzz/fizzBuzz.js
+++ Exercise/FizzBuzz/fizzBuzz.js
@@ -1,24 +1,24 @@
-function fizzBuzz(){
+function fizzBuzz() {
     var output;
     for (var i = 1 ; i<=100 ; i++){
         output = "";
-        if (i % 3 == 0){
-            output+="Fizz";
+        if (i % 3 == 0) {
+            output += "Fizz";
         }
         if (i % 5 == 0) {
-            output+="Buzz";
+            output += "Buzz";
          }
         console.log(output || i);
     }
 }
-function woozyBoozy(config,times){    
+function woozyBoozy(config,times) {    
     var output;
     for(var i = 1; i<=times ; i++){
         output = "";
         for(var prop in config){
-            if(config.hasOwnProperty(prop)){
-                if(i % config[prop] == 0){
-                    output+= prop;
+            if (config.hasOwnProperty(prop)) {
+                if (i % config[prop] == 0) {
+                    output += prop;
                 }
             }
         }
