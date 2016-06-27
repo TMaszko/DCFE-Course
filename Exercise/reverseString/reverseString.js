73a80737a01e72243f1cfe5d25fe460a982fe316
Tomasz Krzyżowski <Tomasz Krzyowski>
2016-06-27 19:34:28 +0200

    Add 2 recursive method

--- Exercise/reverseString/reverseString.js
+++ Exercise/reverseString/reverseString.js
@@ -4,3 +4,19 @@ function reverseString(str){
         output +=str.charAt(i);
     return output
 }
+
+function reverseStringR(str){
+    if(str.length == 1)
+        return str;
+    else {
+        return reverseStringR(str.slice(1)) +str.charAt(0); // reversed Hi =  i+H ,H = str[0]
+    }
+}
+
+function reverseStringR2(str){
+    if(str.length == 1)
+        return str;
+    else {
+        return str.charAt(str.length-1) + reverseStringR2(str.slice(0,str.length-1)); // reversed dwa = a + reversed(dw);
+    }
+}
